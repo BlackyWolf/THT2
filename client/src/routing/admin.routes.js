118092717a71db37f492a96e6de7d5c6673a7fc1
Blackywolf <golgotharae@gmail.com>
2019-10-13 14:44:16 -0500

    Deleting files to move to Blazor

--- client/src/routing/admin.routes.js
+++
@@ -1,5 +0,0 @@
-import AdminDashboard from '../components/admin/pages/AdminDashboard.ui';
-
-export default [
-    { path: '/admin/dashboard', component: AdminDashboard }
-];
--- client/src/store/global/actions.js
+++
@@ -1,8 +0,0 @@
-import {
-    TOGGLE_LOADER
-} from './types';
-
-export const toggleLoader = (showLoader) => ({
-    type: TOGGLE_LOADER,
-    payload: showLoader
-});
